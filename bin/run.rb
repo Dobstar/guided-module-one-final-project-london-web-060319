f77b1faaf31e93792044529f6267df4d3844a86a
github username <github email address>
2019-06-17 21:41:11 +0100

    update cli

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,4 @@
 require_relative '../config/environment'
 cli = CommandLineInterface.new
 cli.greet
+cli.previous_user_reviews
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,9 +4,9 @@ class CommandLineInterface
     end 
     
 
-    def previous_user_reviews(user_response)
-        puts "Need a new coffee shop to chill? Or try a different brew?? We can help you with your decision!"
+    def previous_user_reviews
+        puts "Need a new coffee shop to chill? Or want to try a different brew?? We can help you with your decision, with loads of reviews at our fingertips!"
         puts "Please enter a coffee shop name:"
-        CoffeeShop.find_by(name: coffee_shop_name)
+        #CoffeeShop.find_by(name: coffee_shop)
     end 
 end 
