2eeb85f6c4685040ce9711022039c6d7ad676fcb
github username <github email address>
2019-06-17 13:47:33 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190617122547) do
+ActiveRecord::Schema.define(version: 20190617124522) do
+
+  create_table "coffee_shops", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
